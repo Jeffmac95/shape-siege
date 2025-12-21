4e1ef17286cb02c884bb66d1b46e88daaf2b25fc
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2025-12-21 02:20:40 -0400

    added vars for fps

--- src/main.js
+++ src/main.js
@@ -8,10 +8,22 @@ canvas.tabIndex = 0;
 const game = new Game(canvas, ctx);
 
 let lastTime = 0;
+let fps = 60;
+let frameCount = 0;
+let fpsTime = 0;
 
 function animate(timeStamp) {
     const deltaTime = Math.min(timeStamp - lastTime, 100);
     lastTime = timeStamp;
+
+    frameCount++;
+    fpsTime += deltaTime;
+    if (fpsTime >= 1000) {
+        fps = frameCount;
+        frameCount = 0;
+        fpsTime = 0;
+    }
+    game.fps = fps;
 
     game.update(deltaTime);
     game.render();

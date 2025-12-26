5f6895d17fa8edd92b5777274b0760bf2b774b58
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2025-12-25 23:57:47 -0400

    changed width and height to match game screen

--- src/camera.js
+++ src/camera.js
@@ -1,7 +1,7 @@
 export default class Camera {
-    constructor(canvas, mapWidth, mapHeight) {
-        this.width = 600;
-        this.height = 500;
+    constructor(mapWidth, mapHeight) {
+        this.gameWidth = 600;
+        this.gameHeight = 500;
         this.x = 0;
         this.y = 0;
         this.mapWidth = mapWidth;
@@ -9,10 +9,10 @@ export default class Camera {
     }
 
     follow(target) {
-        this.x = target.x - this.width / 2;
-        this.y = target.y - this.height / 2;
+        this.x = target.x - this.gameWidth / 2;
+        this.y = target.y - this.gameHeight / 2;
 
-        this.x = Math.max(0, Math.min(this.x, this.mapWidth - this.width));
-        this.y = Math.max(0, Math.min(this.y, this.mapHeight - this.height));
+        this.x = Math.max(0, Math.min(this.x, this.mapWidth - this.gameWidth));
+        this.y = Math.max(0, Math.min(this.y, this.mapHeight - this.gameHeight));
     }
 }
